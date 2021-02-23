149abb0ba60fd610db289f3d6f6cf016de9322b7
davidas02 <davidapariciosir@gmail.com>
2021-02-23 11:51:40 +0100

    Banco Programación

---
+++ src/main/java/com/sauces/ud05a05/AppBanco.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sauces.ud05a05;
+
+/**
+ *
+ * @author daw1
+ */
+public class AppBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/com/sauces/ud05a05/Cuenta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sauces.ud05a05;
+
+/**
+ *
+ * @author daw1
+ */
+public class Cuenta {
+    
+}
